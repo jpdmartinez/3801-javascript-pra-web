dfbec1b05f52bfacd0f81f18f08c818d9f01bef2
João Pedro Martinez <martinez.jpd@gmail.com>
2024-10-05 15:53:35 -0300

    Checkbox e lista comprados

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const item = document.getElementById("input-item");
 const botaoSalvarItem = document.getElementById("adicionar-item");
 const listaDeCompras = document.getElementById("lista-de-compras");
+const listaComprados = document.getElementById("lista-comprados");
+let contador = 0;
 
 botaoSalvarItem.addEventListener("click", adicionarItem);
 
@@ -14,7 +16,52 @@ function adicionarItem(evento){
 
     //cria uma nova div, o parágrafo do texto e pega o seu valor.
     const containerNomeDoItem = document.createElement("div");
+
+    const containerCheckbox = document.createElement("div");
+    containerCheckbox.classList.add("container-checkbox");
+
+    //Criando o checkbox
+    const checkboxInput = document.createElement("input");
+    checkboxInput.type = "checkbox";
+    checkboxInput.classList.add("input-checkbox");
+    //id variável do checkbox
+    checkboxInput.id = "checkbox-" + contador++;
+
+    //cria a label do checkbox
+    const checkboxLabel = document.createElement("label");
+    checkboxLabel.setAttribute("for", checkboxInput.id);
+
+    checkboxLabel.addEventListener("click", function (evento){
+        const checkboxInput = evento.currentTarget.querySelector(".input-checkbox");
+        const checkboxCustomizado = evento.currentTarget.querySelector(".checkbox-customizado");
+        const itemTitulo = evento.currentTarget.closest("li").querySelector("#item-titulo");
+
+        if (checkboxInput.checked){
+            checkboxCustomizado.classList.add("checked");
+            itemTitulo.style.textDecoration = "line-through";
+            listaComprados.appendChild(itemDaLista);
+        } else {
+            checkboxCustomizado.classList.remove("checked");
+            itemTitulo.style.textDecoration = "none";
+            listaDeCompras.appendChild(itemDaLista);
+        }
+    })
+
+    //cria o checkbox customizado
+    const checkboxCustomizado = document.createElement("div");
+    checkboxCustomizado.classList.add("checkbox-customizado");
+
+    //adiciona o input e o checkbox customizado dentro da label
+    checkboxLabel.appendChild(checkboxInput);
+    checkboxLabel.appendChild(checkboxCustomizado);
+
+    //adiciona a label dentro do container e o container checkbox dentro do container Nome do Item
+    containerCheckbox.appendChild(checkboxLabel);
+    containerNomeDoItem.appendChild(containerCheckbox);
+
+    //cria o nome do item novo.
     const nomeDoItem = document.createElement("p");
+    nomeDoItem.id = "item-titulo";
     nomeDoItem.innerText = item.value;
 
     // Adiciona o valor do texto dentro do elemento div
